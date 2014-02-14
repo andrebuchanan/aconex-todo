e95067996814917b4ec9058157703f51f5d8de67
rogerano <rogerano@gmail.com>
2014-02-14 21:34:54 +1100

    Initial commit of yeoman scaffolded app

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('aconexTodoApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
